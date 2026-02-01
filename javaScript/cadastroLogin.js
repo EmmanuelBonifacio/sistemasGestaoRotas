dc29cfc12d225d944d1357a95c34febde267c001
EmmanuelBonifacio <bonifacioe092@gmail.com>
2026-02-01 17:48:40 -0300

    criando uma array para salvar os usuarios cadastrados

--- javaScript/cadastroLogin.js
+++ javaScript/cadastroLogin.js
@@ -1,27 +1,31 @@
+// Cadastro de Usuário sao armazenados em um Array
 const listaUsuarios = [];
 
-documen
-  .getElementById(bntCadastrar)
-  .addEventListener("click", function (event) {
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-    const confirmarSenha = document.getElementById("confirmarSenha").value;
-    const tipoConta = document.getElementById("tipoConta").value;
-    if (senha !== confirmarSenha) {
-      alert("As senhas não coincidem. Por favor, tente novamente.");
-      event.preventDefault();
-      return;
-    }
-  });
+document.getElementById("bntCadastrar").addEventListener("click", () => {
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  const confirmarSenha = document.getElementById("confirmarSenha").value;
+  const tipoConta = document.getElementById("tipoConta").value;
 
-const novoUsuario = {
-  nome: nome,
-  email: email,
-  senha: senha,
-  tipoConta: tipoConta,
-};
-listaUsuarios.push(novoUsuario);
-console.log("Usuário cadastrado com sucesso:", novoUsuario);
-console.log("Lista de usuários:", listaUsuarios);
-event.preventDefault();
+  const novoUsuario = {
+    nome: nome,
+    email: email,
+    senha: senha,
+    tipoConta: tipoConta,
+  };
+  listaUsuarios.push(novoUsuario);
+
+  console.log("Usuário cadastrado com sucesso:", novoUsuario);
+  alert("Cadastro realizado com sucesso!");
+  limparFormulario();
+  console.log("Lista de usuários:", listaUsuarios);
+});
+// Função para limpar o formulário após o cadastro
+function limparFormulario() {
+  document.getElementById("nome").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("senha").value = "";
+  document.getElementById("confirmarSenha").value = "";
+  document.getElementById("tipoConta").value = "usuario";
+}

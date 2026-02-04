92f16851f9b8f00b4fcebebdf6f424533f2bfd78
EmmanuelBonifacio <bonifacioe092@gmail.com>
2026-02-04 19:26:09 -0300

    Array para armazenar as rotas otmizadas

--- javaScript/desboardUsuario.js
+++ javaScript/desboardUsuario.js
@@ -1,5 +1,6 @@
 // Array para armazenar as rotas adicionadas
 const rotas = [];
+const rotasOtimizadas = [];
 
 const bntAdiconarRota = document.getElementById("bntAdiconarRota");
 
@@ -14,10 +15,18 @@ if (bntAdiconarRota) {
       bairro: document.getElementById("bairro").value,
       cidade: document.getElementById("cidade").value,
     };
-
     rotas.push(novasRotas);
+    const bntRotaEficiente = document.getElementById("bntRotaEficiente");
+    if (bntRotaEficiente) {
+      bntRotaEficiente.addEventListener("click", function (e) {
+        e.preventDefault();
+      });
+    }
+    rotasOtimizadas.push(rotas);
     console.log(rotas);
     alert("Rota adicionada com sucesso!");
+    console.log(rotasOtimizadas);
+    alert("Funcionalidade de gerar rota mais eficiente em desenvolvimento.");
 
     // Limpar os campos do formulário após adicionar a rota
     document.getElementById("cep").value = "";
